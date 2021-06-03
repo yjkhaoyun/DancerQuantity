f39d72804e2e55b560c5e0fd21c429d104683665
颜金宽 <1281203326@qq.com>
2021-06-04 00:49:16 +0800

    打包就绪

--- src/test/java/com/numtimequantity/quantity/QuantityApplicationTests.java
+++ src/test/java/com/numtimequantity/quantity/QuantityApplicationTests.java
@@ -14,17 +14,17 @@ class QuantityApplicationTests {
 TopSymbolThread topSymbolThread;
     @Test
     void contextLoads() {
-        topSymbolThread.setTopSymbolThreadIf(true);
+        /*topSymbolThread.setTopSymbolThreadIf(true);
         Thread thread = new Thread(topSymbolThread);
-        thread.start();
+        thread.start();*/
     }
     @Test
     void test2(){
-        String arr[] = {Long.toString(new Date().getTime()),"0"};
+        /*String arr[] = {Long.toString(new Date().getTime()),"0"};
         System.out.println(arr.getClass());
         ArrayList<String[]> list = new ArrayList<>();
         list.add(arr);//将指定元素添加到末尾
 
-        System.out.println(list.get(0)[0]);
+        System.out.println(list.get(0)[0]);*/
     }
 }
